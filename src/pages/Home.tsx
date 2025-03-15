d83188dec217876ba3d4aad9b4241072c7512a9e
Zack <zack@kapito.io>
2025-03-15 15:22:42 +0800

    fix: Update initial tab state in Home component from 6 to 0 for correct default tab display.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ import { TransactionStats } from '../components/TransactionStats/TransactionStat
 import { UserTransactionRank } from '../components/UserTransactionRank/UserTransactionRank'
 
 export function Home () {
-  const [tab, setTab] = useState(6)
+  const [tab, setTab] = useState(0)
 
   return (
     <Box p={2}>
