9004deb47f70c0f9983ab13e375df8e8cf5dd0eb
Zack <zack@kapito.io>
2025-03-15 17:11:20 +0800

    chore: Remove urlWithQueryParams utility function as it is no longer needed in the project.

--- src/utils/urlWithQueryParams.ts
+++
@@ -1,8 +0,0 @@
-import qs from 'query-string'
-
-export function urlWithQueryParams (path: string, queryParams: Record<string, unknown>): string {
-  const filteredParams = Object.fromEntries(
-    Object.entries(queryParams).filter(([_, value]) => value)
-  )
-  return `${path}?${qs.stringify(filteredParams)}`
-}
